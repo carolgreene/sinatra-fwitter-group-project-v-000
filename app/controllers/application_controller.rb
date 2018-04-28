010145afc7753f2663a02a8e86246b869c05e1b8
Carol Greene <carolgreene3@gmail.com>
2018-04-28 01:20:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,25 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  get '/' do
+    erb :'homepage'
+  end
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    @user = User.find_by(email: params[:email])
+    session[:user_id] = @user.id
+    redirect '/users/index'
+  end
+
 
 end
